fcd0cdaa134f8400b82f1886f94fb6aec9e214b7
Joanico <joanicobarros@gmail.com>
2016-04-27 11:02:30 +0900

    adding rest_framework in todoapp

--- src/todo/urls.py
+++ src/todo/urls.py
@@ -13,13 +13,20 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import url, include
 from django.contrib import admin
 from todoapp.views import TodoView
+from rest_framework import routers
+from todoapp import views
 
+
+router = routers.DefaultRouter()
+router.register(r'users', views.UserViewSet)
+router.register(r'groups', views.GroupViewSet)
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^$', TodoView.as_view(), name='index')
-
+    url(r'^', include(router.urls)),
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    url(r'^index/', TodoView.as_view(), name='index'),
 ]
